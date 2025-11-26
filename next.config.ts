dc92448d96b0c40a19821c35be2eab8a1cc7bac6
GenicZ <121470168+Geniccc@users.noreply.github.com>
2025-11-26 09:04:27 +0700

    modified dikit

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-      },
-      {
+            {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com', // Domain CDN untuk thumbnail Google
       },
       {
         protocol: 'https',
         hostname: 'googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
       },
     ],
   },
